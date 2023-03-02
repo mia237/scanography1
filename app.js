bf54fb72083bf057626bcfde856904a73c2e71af
mia237 <124846443+mia237@users.noreply.github.com>
2023-03-02 00:51:12 -0500

    Add files via upload

--- app.js
+++ app.js
@@ -71,3 +71,150 @@ if (x.innerHTML === "Stop scan ") {
 
 
 
+
+    function myFunctionphotoR() {
+      document.getElementById("demophotoR").innerHTML += "T ";
+    }
+  
+
+
+function myFunctionf() {
+  document.getElementById("demof").innerHTML += " abstract ";
+}
+
+
+function myFunctionmove() {
+  document.getElementById("demomove").innerHTML += "movements ";
+}
+
+
+            function myFunctionhead() {
+              document.getElementById("sub1").innerHTML = "By dragging the object vertically as it scans, you can stretch the object.";
+            }
+
+            function myFunctionhead1() {
+              document.getElementById("sub3").innerHTML = "By dragging the object horizontally as it scans, you can warp the object.";
+            }
+
+            function myFunctionhead2() {
+              document.getElementById("sub4").innerHTML = "By picking up and placing the object in a new position as it scans, you can relocate the object.";
+            }
+            function myFunctionhead3() {
+              document.getElementById("sub6").innerHTML = "By scanning see-through objects, such as saran-wrap, platic, or glass, you can add texture.";
+            }
+            function myFunctionhead4() {
+              document.getElementById("sub8").innerHTML = "By placing objects ontop of eachother, you can add layers to the scan.";
+            }
+          
+            function myFunctionHi() {
+              let id = null;
+              const elem = document.getElementById("animate");   
+              let pos = 0;
+              clearInterval(id);
+              id = setInterval(frame, 10);
+              function frame() {
+                if (pos == 370) {
+                  clearInterval(id);
+                  elem.style.opacity = 0;
+                } else {
+                  pos++; 
+                  elem.style.top = pos + "px"; 
+                  elem.style.left = pos + "-50px"; 
+                  elem.style.opacity = 1;
+                }
+              }
+            }
+        
+
+function myFunctionHi2() {
+  let id = null;
+  const elem = document.getElementById("animate2");   
+  let pos = 90;
+  clearInterval(id);
+  id = setInterval(frame, 10);
+  function frame() {
+    if (pos == 490) {
+      clearInterval(id);
+      elem.style.opacity = 0;
+    } else {
+      pos++; 
+      elem.style.top = pos + "px"; 
+      elem.style.left = pos + "px"; 
+      elem.style.opacity = 1;
+    }
+  }
+}
+
+
+function myFunctionrel() {
+  var x = document.getElementById("myDIVrel");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
+
+
+function myFunctionrel4() {
+  var x = document.getElementById("myDIVrel4");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
+
+function myFunctionrel1() {
+  var x = document.getElementById("myDIVrel1");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+
+
+
+function myFunctionrel3() {
+  var x = document.getElementById("myDIVrel3");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+
+    function myFunctionrel2() {
+      var x = document.getElementById("myDIVrel2");
+      if (x.style.display === "none") {
+        x.style.display = "block";
+      } else {
+        x.style.display = "none";
+      }
+    }
+
+    function myFunctionrel6() {
+      var x = document.getElementById("myDIVrel6");
+      if (x.style.display === "none") {
+        x.style.display = "block";
+      } else {
+        x.style.display = "none";
+      }
+    }
+
+    function myFunctionsub55() {
+      document.getElementById("sub55").innerHTML += " <br> Through combining these different movements togehter, you can create some really intersting and abstract works. ";
+    }
+
+    function myFunctionworks() {
+      document.getElementById("demoworks").innerHTML += " &nbsp;&nbsp;w&nbsp;&nbsp;o&nbsp;&nbsp;r&nbsp;&nbsp;k&nbsp;&nbsp;s ";
+    }
+
+    function myFunctionhh11() {
+      document.getElementById("hh11111").innerHTML += " works ";
+    }
+    
+    function myFunctionhb() {
+      document.getElementById("hb").innerHTML += "<br> artists' ";
+    }
